47151d04272e2ae761032fae9354214b47faea58
shoaebns <143832654+shoaebns@users.noreply.github.com>
2023-09-24 22:54:24 -0700

    Chap3

---
+++ Zybooks/3_14_Simple_statistics.py
@@ -0,0 +1,11 @@
+num1 = float(input())
+num2 = float(input())
+num3 = float(input())
+num4 = float(input())
+
+''' Type your code here. '''
+pdt = num1 * num2 * num3 * num4
+avg = (num1 + num2 + num3 + num4)/4
+
+print(f'{pdt:.0f} {avg:.0f}')
+print(f'{pdt:.3f} {avg:.3f}')
