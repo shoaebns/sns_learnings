f4af23b096e6feea50536c13327f581fce2b0eab
shoaebns <143832654+shoaebns@users.noreply.github.com>
2023-09-25 18:35:22 -0700

    Interstate

---
+++ Zybooks/4_14_1_Interstates.py
@@ -0,0 +1,28 @@
+highway_number = int(input())
+if highway_number < 1 or highway_number > 999 or highway_number%100 == 0 :
+    print ('{} is not a valid interstate highway number.'.format (highway_number))
+else:
+    # Type + serving
+    if highway_number > 100:
+        road_type = 'auxiliary'
+        serving = str(highway_number % 100)
+    else:
+        road_type = 'primary'
+        serving = ''
+# Direction
+    if highway_number % 2 == 0:
+        going = 'east/west.'
+    else:
+        going = 'north/south.'
+
+# Create output
+
+    output = ['I-{}'.format(highway_number), 'is', road_type + ',']
+
+    if serving:
+
+        output.extend(['serving', 'I-{},'.format(serving)])
+
+    output.extend(['going', going])
+
+    print(' '.join(output)) 
