ea07e226746b6f12dd025f5e12759e352f46aa77
shoaebns <143832654+shoaebns@users.noreply.github.com>
2023-09-26 00:16:25 -0700

    Triangle

--- Turtle_Grp/Turtle3.py
+++ Turtle_Grp/Turtle3.py
@@ -3,9 +3,9 @@ import turtle
 # don't name the file a turtle.py -- it causes a name conflict error.
 
 # Named constants
-NUM_CIRCLES = 36  # Number of circles to draw
+NUM_CIRCLES = 180  # Number of circles to draw
 RADIUS = 100  # Radius of each circle
-ANGLE = 10  # Angle to turn
+ANGLE = 2  # Angle to turn
 ANIMATION_SPEED = 0  # Animation speed
 COLORS = ["red", "green", "blue"]
 
@@ -23,5 +23,10 @@ for x in range(NUM_CIRCLES):
     color_index += 1
     if color_index >= len(COLORS):
         color_index = 0
+for i in range(3):
+
+    t.forward(200)
+    t.left(120)
+    
 
 t.screen.mainloop()
