331b84581c347e1d610b999d375a677cf02260f2
shoaebns <143832654+shoaebns@users.noreply.github.com>
2023-09-27 22:46:00 -0700

    OK

---
+++ Zybooks/6_20_1.py
@@ -0,0 +1,14 @@
+def fibonacci(n):
+    # Type your code here. 
+    if n == 0:
+        return 0
+    elif n==1:
+        return 1
+    elif n<0:
+        return -1
+    return fibonacci(n-1)+fibonacci(n-2)
+
+
+if __name__ == '__main__':
+    start_num = int(input())
+    print(f'fibonacci({start_num}) is {fibonacci(start_num)}')
