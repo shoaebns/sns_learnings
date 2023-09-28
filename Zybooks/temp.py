0aa7a4682361085ee5db192b84fee187d6961e3f
shoaebns <143832654+shoaebns@users.noreply.github.com>
2023-09-27 18:16:48 -0700

    modify

--- Zybooks/temp.py
+++ Zybooks/temp.py
@@ -1,17 +1,14 @@
-# Define your functions here.
 def int_to_reverse_binary(x):
     str= ""
-    while(x > 0):
+    while(x!=0):
         if(x%2 == 0):
             str += "0"
         else:
             str += "1"
-        x= x//2
-    return str
+        x= x>>1
+    return string_reverse(str)
 def string_reverse(input_string):
     return input_string[::-1]
 if __name__ == '__main__':
     user_input = int(input())
-    binary_reverse = int_to_reverse_binary(user_input)
-    actual_binary = string_reverse(binary_reverse)
-    print(actual_binary)
+    print(int_to_reverse_binary(user_input))
