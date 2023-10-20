25028eb4801e7f477c34d97f9a73e09b8e2ae57f
shoaebns <143832654+shoaebns@users.noreply.github.com>
2023-10-19 22:57:25 -0700

    ..

---
+++ Zybooks/Ch_8_to_14/10_11_1.py
@@ -0,0 +1,5 @@
+def __init__(self, item_name="Water", amount_fat=0.0, amount_carbs=0.0, amount_protein=0.0):
+        self.name = item_name
+        self.fat = amount_fat
+        self.carbs = amount_carbs
+        self.protein = amount_protein
