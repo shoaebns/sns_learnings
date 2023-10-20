036bdb050d818b4385e99c345cd2d1525571d4e0
shoaebns <143832654+shoaebns@users.noreply.github.com>
2023-10-19 19:35:07 -0700

    Updt

---
+++ Zybooks/Ch_8_to_14/10_6_2.py
@@ -0,0 +1,3 @@
+def __init__(self, minutes=0, messages=0):
+        self.num_mins=minutes
+        self.num_messages=messages
---
+++ Zybooks/Ch_8_to_14/10_8_2.py
@@ -0,0 +1,2 @@
+def __str__(self):
+        return f"Year: {self.year_made}, VIN: {self.car_vin}"
