527b169590ae7dc7f65fbf03d43b971ec4628c82
shoaebns <143832654+shoaebns@users.noreply.github.com>
2023-10-31 00:55:28 -0700

    ..

--- Zybooks/Ch_8_to_14/11_9_1.py
+++ Zybooks/Ch_8_to_14/11_9_1.py
@@ -3,37 +3,47 @@ class Plant:
         self.plant_name = plant_name
         self.plant_cost = plant_cost
 
-    def print_info(self):
-        print(f'   Plant name: { self.plant_name }')
-        print(f'   Cost: { self.plant_cost }')
-
+    def print_info(self, index):
+        print(f'Plant {index} Information:')
+        print(f'   Plant name: {self.plant_name}')
+        print(f'   Cost: {self.plant_cost}')
+        print()
+        
 class Flower(Plant):
     def __init__(self, plant_name, plant_cost, is_annual, color_of_flowers):
-        Plant.__init__(self, plant_name, plant_cost)
+        super().__init__(plant_name, plant_cost)
         self.is_annual = is_annual
         self.color_of_flowers = color_of_flowers
 
-    def print_info(self):
-        print(f'   Plant name: { self.plant_name }')
-        print(f'   Cost: { self.plant_cost }')
-        print(f'   Annual: { self.is_annual }')
-        print(f'   Color of flowers: { self.color_of_flowers }')
+    def print_info(self, index):
+        print(f'Plant {index} Information:')
+        print(f'   Plant name: {self.plant_name}')
+        print(f'   Cost: {self.plant_cost}')
+        print(f'   Annual: {str(self.is_annual).lower()}')
+        print(f'   Color of flowers: {self.color_of_flowers}')
+        print()
 
-# TODO:  Define the print_list() function that prints a list of plant (or flower) objects 
-#def print_list():
-
+def print_list(garden):
+    for index, plant in enumerate(garden, start=1):
+        plant.print_info(index)
 
 if __name__ == "__main__":
-
-    # TODO: Declare a list called my_garden that can hold object of type plant
-
+    my_garden = []
     user_string = input()
-    
+
     while user_string != '-1':
-        # TODO: Check if input is a plant or flower
-        #       Split the user_string input into variables - plant_name, plant_cost, color_of_flowers, is_annual
-        #       Store as a plant object or flower object
-        #       Add to the list my_garden
+        plant_data = user_string.split()
+        plant_name = plant_data[1]
+        plant_cost = plant_data[2]
+        
+        if plant_data[0] == 'plant':
+            plant = Plant(plant_name, plant_cost)
+        elif plant_data[0] == 'flower':
+            is_annual = plant_data[3] == 'true'
+            color_of_flowers = plant_data[4]
+            plant = Flower(plant_name, plant_cost, is_annual, color_of_flowers)
+        
+        my_garden.append(plant)
         user_string = input()
 
-    # TODO: Call the print_list() function to print my_garden
+    print_list(my_garden)
